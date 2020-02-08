b01f6e49e09c24fd101f0da7a3de9e7a81bb0230
kdokic1 <kdokic1@etf.unsa.ba>
2020-02-08 15:47:25 +0100

    napisan konstruktor, geteri i seteri za klasu Subject

--- src/ba/unsa/etf/Subject.java
+++ src/ba/unsa/etf/Subject.java
@@ -2,5 +2,36 @@ package ba.unsa.etf;
 
 public class Subject {
     private String subjectName;
-    private Person teacher;
+    private String teacher;
+    private String classroom;
+
+    public Subject(String subjectName, String teacher, String classroom) {
+        this.subjectName = subjectName;
+        this.teacher = teacher;
+        this.classroom = classroom;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
 }
