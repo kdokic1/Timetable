fd55d8048e6631222ecc223806a1c21ab8499197
kdokic1 <kdokic1@etf.unsa.ba>
2020-02-09 18:20:11 +0100

    dodane jos dvije tabele u bazu ( timetable i timetable_field)

--- timetable.db.sql
+++ timetable.db.sql
@@ -15,6 +15,26 @@ CREATE TABLE IF NOT EXISTS "user" (
 	"password"	TEXT,
 	PRIMARY KEY("username")
 );
+CREATE TABLE IF NOT EXISTS "timetable" (
+    "timetable_name" TEXT,
+    "user" TEXT,
+    PRIMARY KEY("timetable_name"),
+    FOREIGN KEY("user") REFERENCES "user"("username")
+);
+CREATE TABLE IF NOT EXISTS "timetable_field" (
+    "timetable" TEXT,
+    "user" TEXT,
+    "day" TEXT,
+    "ordinal_number" INTEGER,
+    "subject" TEXT,
+    "starts" time,
+    "ends" time,
+    FOREIGN KEY ("timetable") REFERENCES "timetable"("timetable_name"),
+    FOREIGN KEY ("user") REFERENCES "user"("username"),
+    FOREIGN KEY ("subject") REFERENCES "subject"("subject_name")
+);
 INSERT INTO "user" VALUES ('kdokic1','Kanita','Đokić','kdokic1@etf.unsa.ba','Uquahlahying18523');
 INSERT INTO "subject" VALUES ('math','kdokic1','Muslija Omerović','classroom');
+INSERT INTO "timetable" VALUES ('novi','kdokic1');
+INSERT INTO "timetable_field" VALUES ('novi','kdokic1','mon','2','math','11:00:00','12:00:00');
 COMMIT;
